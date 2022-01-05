d2e058cb9d8f3137678ed7bd8e9fda82c7193dc1
Christos <xristos94sideris@gmail.com>
2022-01-05 12:48:40 +0200

    ReactJS tutorial, UseState exercise, counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const useState = React.useState;
+
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  const onButtonClick = () => {
+    setCount(count + 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="content">
+      <button onClick={onButtonClick}>Click me!</button>
+      <h1>Count is: {count}</h1>
     </div>
   );
 }
-
-export default App;
